689a43752b166d3098e59db54df094d79c967601
Koustav Das <kdkdas8@gmail.com>
2024-02-29 03:03:42 +0530

    -_-

---
+++ helo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("Hello World!!!!!");
+printf("\nEnter a value ->>>");
+int num1, num2, num3;
+scanf("%d %d %d", &num1, &num2, &num3);
+return num1+num2 ;
+}
+printf(main());
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 (function () {
-  console.log("DSA IN TYPESCRIPT");
+  for (let i = 1; i < 10; i++) {
+    let str: string = "";
+    for (let j = 10 - i; j > 0; j--) {
+      str += " ";
+    }
+    for (let z = 2 * i - 1; z > 0; z--) {
+      str += "*";
+    }
+    console.log(str);
+  }
+  console.log(" DSA IN TYPESCRIPT");
 })();
 
-function add(n1: number): number {
+const p1: number = performance.now();
+console.log(addUpto(1));
+const p2: number = performance.now();
+console.log(p2 - p1);
+
+function addUpto(n1: number): number {
   let number = 0;
   for (let i = 0; i <= n1; i++) {
     number += i;
@@ -10,7 +25,38 @@ function add(n1: number): number {
   return number;
 }
 
-const p1: number = performance.now();
-console.log(add(10));
-const p2: number = performance.now();
-console.log(p2 - p1);
+const str: string =
+  "sdlkad5467jsajd  SDADSAD   dsakj          dlasjkdla                      12341234";
+
+//get the number from any string
+console.log(
+  str
+    .split("")
+    .filter((el) => parseInt(el))
+    .join("")
+);
+
+//count common words
+
+function charCounter(str: string): {} {
+  const result: {
+    [key: string]: number;
+  } = {};
+
+  for (let i: number = 0; i < str.length; i++) {
+    let ch: string = str[i].toLowerCase();
+    const code: number = ch.charCodeAt(0);
+    if ((code > 47 && code < 58) || (code > 96 && code < 123)) {
+      result[ch] = ++result[ch] || 1;
+    }
+  }
+  return result;
+}
+
+function logger(value: string, message?: string): void {
+  message ? console.log(message, value) : console.log(value);
+}
+
+logger("asdasd");
+
+console.log(charCounter(str));
