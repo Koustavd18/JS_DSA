612ee612c024ac681e89c8fe54ae92925042c485
Koustav Das <kdkdas8@gmail.com>
2024-04-07 22:06:01 +0530

    updates

--- src/recursion.ts
+++ src/recursion.ts
@@ -130,7 +130,81 @@ function fib(n: number): number {
   return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fib(4)); // 3
-console.log(fib(10)); // 55
-console.log(fib(28)); // 317811
-console.log(fib(35)); // 9227465
+// console.log(fib(4)); // 3
+// console.log(fib(10)); // 55
+// console.log(fib(28)); // 317811
+// console.log(fib(35)); // 9227465
+
+function reverse(str: string): string {
+  str = str.trim();
+  if (str.length === 0) {
+    return " ";
+  }
+  const strArr = str.split("");
+  const last = strArr[str.length - 1];
+  const result: string[] = [];
+  result.push(last);
+  let str1: string = strArr.slice(0, str.length - 1).join("");
+  return result.concat(reverse(str1)).join("").trim();
+}
+
+// if(str.length <= 1) return str;
+// return reverse(str.slice(1)) + str[0];
+
+// console.log(reverse("awesome")); // 'emosewa'
+// console.log(reverse("rithmschool")); // 'loohcsmhtir'
+
+function isPalindrome(str: string): boolean {
+  if (str.length <= 1) return true;
+  if (str.length == 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) {
+    return isPalindrome(str.slice(1, -1));
+  }
+  return false;
+}
+
+// console.log(isPalindrome("awesome")); // false
+// console.log(isPalindrome("foobar")); // false
+// console.log(isPalindrome("tacocat")); // true
+// console.log(isPalindrome("amanaplanacanalpanama")); // true
+// console.log(isPalindrome("amanaplanacanalpandemonium")); // false
+
+function someRecursive(arr: number[], fn: (val: number) => boolean) {
+  if (arr.length === 0) return false;
+
+  if (fn(arr[0])) return true;
+
+  return someRecursive(arr.slice(1), fn);
+}
+
+const isOdd = (val: number) => val % 2 !== 0;
+
+// console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+// console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+// console.log(someRecursive([4, 6, 8], isOdd)); // false
+// console.log(someRecursive([4, 6, 8], (val: number) => val > 10)); // false
+
+function flatten(oldArr: any): number[] {
+  let newArr: number[] = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+// console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([[1], [2], [3]])); // [1,2,3]
+// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
+
+//TODO SOlve this
+// function capitalizeFirst(arr: string[]) {
+//   const newArr: string[] = [];
+//   if (arr.length === 0) return newArr;
+// }
+
+// console.log(capitalizeFirst(["car", "taco", "banana"])); // ['Car','Taco','Banana']
