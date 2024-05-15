30fef56918537b574153549bceaaa55fd64591cb
Koustav Das <kdkdas8@gmail.com>
2024-05-15 23:44:02 +0530

    New

---
+++ src/data_structures/bst.ts
@@ -0,0 +1,17 @@
+console.log("BINARY SEARCH TREE");
+
+class TreeNode {
+  [x: string]: any;
+  constructor(value: string | number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  [x: string]: any;
+  constructor() {
+    this.root = null;
+  }
+}
